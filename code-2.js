bb2cb276507703439b61649f70cd6d0f0c0d70af
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-01-26 18:04:21 -0800

    Code 2

---
+++ code-2.js
@@ -0,0 +1,21 @@
+// Analyzing Code: More Lines
+
+// Assignment A 
+
+function setup() {
+  createCanvas(340, 340);
+}
+function draw() {
+  line(170, 170, random(width), random(height));
+}
+
+// Assignment B 
+
+function setup() {
+  createCanvas(340, 340);
+}
+function draw() {
+  line(340, 0, random(width), random(height));
+}
+
+// Challenge C: Cannot figure this one out.
