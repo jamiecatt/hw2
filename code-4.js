6eaca4c6461baf0f52f3cd8753e503f76ef6ff38
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-01-28 21:32:55 -0800

    Code 4

--- code-4.js
+++ code-4.js
@@ -1,5 +1,16 @@
 // Modern-Day Mondrian
 
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(255);
+  line (100, 400, 100, 200);
+  line (20, 500, 20, 0);
+  line (0, 0, 0, 400);
+  line (500, 55, 10, 55);
+
 // Rectangle
       strokeWeight (4);
       rect(100, 100, 55, 100);
@@ -21,7 +32,7 @@
       fill (255, 255, 0);
     
 // Rectangle
-      strokeWeight (1);
+      strokeWeight (4);
       rect(300, 0, 55, 55);
       fill (255, 255, 255);
   
@@ -29,3 +40,5 @@
       strokeWeight (4);
       rect(0,0, 100, 100);
       fill (255, 255, 0);
+	
+}
